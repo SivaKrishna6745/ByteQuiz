62e05843ac679c0737cd686ae5b1f7efc233494b
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-07-05 13:05:03 +0530

    feat: integrate answer prop in QuizBoard and FlashCard with flip state style: enhance visual layout and interactions

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type FlashCardProps = {
     question: string;
@@ -6,8 +6,22 @@ type FlashCardProps = {
     category?: string;
 };
 
-const FlashCard = ({ question }: FlashCardProps) => {
-    return <li>{question}</li>;
+const FlashCard = ({ question, answer }: FlashCardProps) => {
+    const [isFlipped, setIsFlipped] = useState<boolean>(false);
+    const toggleFlip = () => {
+        setIsFlipped(!isFlipped);
+    };
+    return (
+        <li
+            className={`flashcard mb-6  text-lg text-gray-900 p-4 rounded-lg hover:shadow-xl ${
+                isFlipped ? 'flipped' : ''
+            }`}
+            onClick={toggleFlip}
+        >
+            <div className={`flashcard-front flex justify-center items-center`}>{question}</div>
+            <div className={`flashcard-back flex justify-center items-center`}>{answer}</div>
+        </li>
+    );
 };
 
 export default FlashCard;
--- src/components/QuizBoard.tsx
+++ src/components/QuizBoard.tsx
@@ -4,9 +4,9 @@ import FlashCard from './FlashCard';
 
 const QuizBoard = () => {
     return (
-        <ul>
+        <ul className="bg-slate-200/5 backdrop-blur-sm p-8 rounded-lg shadow-lg columns-3 md:columns-2 sm:columns-1 gap-x-8">
             {quizData.map((quizItem) => (
-                <FlashCard key={quizItem.id} question={quizItem.question} />
+                <FlashCard key={quizItem.id} question={quizItem.question} answer={quizItem.answer} />
             ))}
         </ul>
     );
