b4936165952a0d48188dec187d031f670f4d85fa
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-07-05 15:57:38 +0530

    feat(audio): change flip sound effect file refactor: simplify mute/unmute button logic chore: clean up unused imports and styles

--- src/components/QuizBoard.tsx
+++ src/components/QuizBoard.tsx
@@ -7,8 +7,18 @@ const QuizBoard = () => {
     const [isMuted, setIsMuted] = useState<boolean>(false);
 
     return (
-        <div className="relative flex gap-4">
-            <ul className="bg-slate-200/5 backdrop-blur-sm p-8 rounded-lg shadow-lg columns-3 md:columns-2 sm:columns-1 gap-x-8">
+        <>
+            <button
+                className={`absolute top-20 right-20 z-10 mute-button self-start flex items-center hover:scale-105 ${
+                    isMuted ? 'text-red-500 hover:text-red-400' : 'text-green-500 hover:text-green-400'
+                }`}
+                title={isMuted ? 'click to unmute' : 'click to mute'}
+                onClick={() => setIsMuted(!isMuted)}
+            >
+                {isMuted ? <GoUnmute /> : <GoMute />}
+                <span className="ml-2 text-sm">{isMuted ? 'Muted' : 'Sound On'}</span>
+            </button>
+            <ul className="bg-slate-200/5 backdrop-blur-sm p-8 rounded-lg shadow-lg grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-x-8">
                 {quizData.map((quizItem) => (
                     <FlashCard
                         key={quizItem.id}
@@ -18,17 +28,7 @@ const QuizBoard = () => {
                     />
                 ))}
             </ul>
-            <button
-                className={`mute-button self-start flex items-center hover:scale-105 ${
-                    isMuted ? 'text-red-500 hover:text-red-400' : 'text-green-500 hover:text-green-400'
-                }`}
-                title={isMuted ? 'click to unmute' : 'click to mute'}
-                onClick={() => setIsMuted(!isMuted)}
-            >
-                {isMuted ? <GoUnmute /> : <GoMute />}
-                <span className="ml-2 text-sm">{isMuted ? 'Muted' : 'Sound On'}</span>
-            </button>
-        </div>
+        </>
     );
 };
 
